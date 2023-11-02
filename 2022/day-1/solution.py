3abc7007d9c184024136b6a0cb95252957af8489
Tolga <tolgacnunal@gmail.com>
2023-11-02 17:09:54 +0100

    solution

--- 2022/day-1/solution.py
+++ 2022/day-1/solution.py
@@ -1,4 +1,5 @@
 import os
+import heapq
 
 INPUT_FILE_LOCATION = os.path.join(os.path.dirname(__file__), "input")
 
@@ -8,10 +9,36 @@ def parse_input():
     return file_content
 
 def get_part_one_solution(input) -> str:
-    return "not solved yet."
+    splitted_calories = input.split(os.linesep)
+    i = 0
+    sum = 0
+    max_sum = 0
+    while i < len(splitted_calories):
+        if splitted_calories[i] == '':
+            max_sum = max(sum, max_sum)
+            sum = 0
+        else:
+            sum += int(splitted_calories[i])
+        i += 1
+        
+    return max_sum
 
 def get_part_two_solution(input) -> str:
-    return "not solved yet."
+    splitted_calories = input.split(os.linesep)
+    top_3 = []
+    i = 0
+    sum_elf = 0
+    while i < len(splitted_calories):
+        if splitted_calories[i] == '':
+            heapq.heappush(top_3, sum_elf)
+            if len(top_3) > 3:
+                heapq.heappop(top_3)
+            sum_elf = 0
+        else:
+            sum_elf += int(splitted_calories[i])
+        i += 1
+        
+    return sum(top_3)
 
 def main():
     input = parse_input()
